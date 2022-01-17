675b96ef5b773faab0ee15e7050a92a1e90431ce
MariusChivu <marius.chivu95@yahoo.com>
2022-01-17 22:19:29 +0200

    update

--- public/js/customJs.js
+++ public/js/customJs.js
@@ -75,3 +75,7 @@ window.onscroll = function() {
 	}
 };
 
+function toTop() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
